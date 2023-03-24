bc25dcdef4347b84daf536e505b3b8edd4c1be60
rmattos <rcm2005@icloud.com>
2023-03-23 23:21:34 -0300

    adicionando arquivo de correção de bus

---
+++ prototipo_de_debug.py
@@ -0,0 +1,65 @@
+print('Insira apenas números entre 0 a 99')
+a = int(input("Digite o primeiro número: ")) # digitar primeiro numero
+b = int(input("Digite outro número: ")) # digitar segundo numero
+
+car1 = 0
+car2 = 0
+car3 = 0
+car4 = 0
+numero = 0
+numero1 = 0
+numero2 = 0
+
+if a < 0 or a > 99 or b < 0 or b > 99:
+	print('apenas números inteiros de 0 a 99')
+else:
+	
+    cara1 = a // 10
+    car2 = a % 10
+
+
+
+
+car1 = car1 *10
+
+numero1 = car1 = car2
+
+
+
+
+car3 = b // 10
+car4 = b % 10  
+
+car3 = car3 * 10
+
+numero2 = car3 + car4
+
+
+
+
+
+
+a_atualizado= [car_1,car_2]
+
+
+
+
+(1, 3)
+
+
+
+
+numero = a + b
+
+
+dig1 = numero // 100
+resto = numero % 100
+dig2 = resto // 10
+dig3 = resto % 10
+
+
+print(f'a soma dos numeros é:{numero}') 
+
+
+
+
